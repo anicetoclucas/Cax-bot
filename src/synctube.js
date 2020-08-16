592128b7aad6ccfccab32d61b3dadbe3036fb355
Tempting Badger <anicetoclucas@gmail.com>
2020-08-16 00:19:39 -0300

    Cleaning synctube and making faster

--- src/synctube.js
+++ src/synctube.js
@@ -3,28 +3,16 @@ const puppeteer = require('puppeteer');
 const base_url_synctube = 'https://sync-tube.de/create';
 
 async function changeSettings(page){
-    await page.waitForSelector('#btnSettings');
-    
-    let notError=true;
-    while (notError) {
-        try{
-            await page.click('#btnSettings');
-            console.log("[changeSettings] Esperando abrir a caixa de settings");
-            await page.waitForSelector('.settings_visible');
-            notError=false;
-        }catch(err){
-            console.log("[changeSettings] Erro. Clicando novamente em #btnSettings");
-            notError=true;
-        }
-    };
+    await page.waitForSelector('li > span');
+    await page.click('#btnSettings');
 
     console.log("[changeSettings] Clicando nos botões");
+
     await page.click("tr[pid='rem'] td div[id='0']");
     await page.click("tr[pid='move'] td div[id='0']");
     await page.click("tr[pid='play'] td div[id='0']");
     await page.click("tr[pid='seek'] td div[id='0']");
     await page.click("tr[pid='skip'] td div[id='0']");
-    await page.click("tr[pid='rem'] td div[id='0']");
     await page.click("tr[pid='kick'] td div[id='0']");
 
     await page.click(".btnClose");
@@ -33,17 +21,17 @@ async function changeSettings(page){
 
 async function searchVideo(page, url){
     console.log('[searchVideo] Procurando video');
+
     await page.focus('.searchInput');
     await page.keyboard.type(url);
-    console.log('[searchVideo] Pesquisou o link');
-    await page.waitForSelector('.search_result');
-    console.log('[searchVideo] Resultado apareceu');
-    await page.click('.search_result > img');
+    await page.keyboard.press('Enter');
+
     console.log('[searchVideo] Clicou');
 }
 
 exports.synctube = async function (url_youtube){
     const browser = await puppeteer.launch({headless: true, defaultViewport: {width:1000, height:1000}, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    // const browser = await puppeteer.launch({ headless: false, executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'}); //For debugging
     const page = await browser.newPage();
     await page.goto(base_url_synctube);
     
